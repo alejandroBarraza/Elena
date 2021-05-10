80cb8e504d372e9416bf3c8fdb10ade82b55668c
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-05-09 22:29:25 -0400

    fix(fix anchor redirect section)

--- main.js
+++ main.js
@@ -2,9 +2,17 @@
 const buttonBurguer = document.querySelector(".burguer-container");
 const burguer = document.querySelector(".burguer");
 const navBurguer = document.querySelector(".nav-links");
+const links = document.querySelectorAll("a");
 
 //event listener
 buttonBurguer.addEventListener("click", () => {
   navBurguer.classList.toggle("open");
   burguer.classList.toggle("openCross");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    navBurguer.classList.toggle("open");
+    burguer.classList.toggle("openCross");
+  });
 });
